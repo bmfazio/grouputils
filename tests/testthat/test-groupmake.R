126ea570acaaf7fb1c7b1846d1c531f7a0b89398
Luna Fazio <bmfaziol@gmail.com>
2023-07-01 13:51:52 +0200

    also test that no group exceeds target size

--- tests/testthat/test-groupmake.R
+++ tests/testthat/test-groupmake.R
@@ -1,4 +1,5 @@
-test_that("Every person is assigned at most one group", {
+# Takes about 45 seconds to run
+test_that("Everyone assigned at most one group, no group exceeds target size", {
   conditions <- expand.grid(groups = 5:10,
                             people = seq(5, 40, 5),
                             size = 2:6,
@@ -7,8 +8,12 @@ test_that("Every person is assigned at most one group", {
 
   set.seed(1249238)
   sims <- apply(conditions, 1, \(x) {
-    max(apply(groupmake(sim_data(x[1], x[2], x[4]), x[3]), 2, sum))
+    result <- groupmake(sim_data(x[1], x[2], x[4]), x[3])
+    c(
+      max_groups = max(apply(result, 2, sum)) <= 1,
+      max_size = max(apply(result, 1, sum)) <= x[3]
+    )
   })
 
-  expect_equal(sims, rep(1, nrow(conditions)), ignore_attr = TRUE)
+  expect_true(all(sims))
 })
